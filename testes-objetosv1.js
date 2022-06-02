38a930c789d9ee0f2fb2539ba68cca070b873bdf
krhistopher <krhiskk@gmail.com>
2022-06-02 20:01:46 -0300

    ok testes

--- testes-objetosv1.js
+++ testes-objetosv1.js
@@ -2,7 +2,7 @@ const colecionador = {
     nome: "Krhistopher",
     figurinhas: "3",
     data: "22/01/1998",
-    EstadoDasFigurinhas: ["Conservado","Depreciado","Estragado","Invalido"],
+    EstadoDasFigurinhas: ["Conservadas","Depreciadas","Estragadas","Invalidas"],
     utilizadores: [{
         nomeUtilizadores: ["Gabriel", "Jonathan", "Fidalgo", "Joatham"],
         quantidade: 4
@@ -16,7 +16,10 @@ const colecionador = {
 
 colecionador.EstadoDasFigurinhas.forEach(buscandoFigurinhas => console.log(buscandoFigurinhas))
 
+
 console.log(`Quanto valia as figurinhas R$${colecionador.dinheiroEmConta}`)
 colecionador.guardandoDinheiro(100)
+
+console.log(`Atualmente as figurinhas estão: ${colecionador.EstadoDasFigurinhas[0]}`)
 console.log(`Quanto está valendo as figurinhas atualmente R$${colecionador.dinheiroEmConta}`)
 
