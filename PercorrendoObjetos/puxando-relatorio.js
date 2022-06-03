a9aa97c44250d1dc21b72bc1c9c02304fda13ae5
krhistopher <krhiskk@gmail.com>
2022-06-02 22:31:22 -0300

    atualizando

--- PercorrendoObjetos/puxando-relatorio.js
+++ PercorrendoObjetos/puxando-relatorio.js
@@ -31,6 +31,7 @@ for (let info in cliente){
         continue
     } else {
         relatorio += ` 
+        // cliente[info] aqui ele ta percorrendo tudo, como não tem como colocarr cliente.nome/cliente.email a gente coloca cliente[info] para percorrer o array e trazer o valor.
         ${info} ==> ${cliente[info]}
         `
     }
